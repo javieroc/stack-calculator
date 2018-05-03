34eb9f92fa1f5b6a079afe5d53e407c31dac3468
Javier Ocampo <ferroxido@gmail.com>
2018-05-02 22:02:01 -0300

    Add layout

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './Button';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  top: {
+    paddingTop: 20,
+  },
+  bottom: {
+    flex: 1,
+  },
+  number: {
+    color: '#70A649',
+    backgroundColor: '#485058',
+    textAlign: 'right',
+    padding: 8,
+    fontSize: 24,
+    fontWeight: 'bold',
+    borderBottomWidth: 1,
+    borderColor: '#70A649',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: '#70A649',
   },
 });
 
 const App = () => (
   <View style={styles.container}>
-    <Text>Hello world!</Text>
-    <Text>Changes you make will automatically reload.</Text>
-    <Text>Shake your phone to open the developer menu.</Text>
+    <View style={styles.top}>
+      <Text style={styles.number}>0</Text>
+      <Text style={styles.number}>0</Text>
+      <Text style={styles.number}>0</Text>
+      <Text style={styles.number}>0</Text>
+    </View>
+    <View style={styles.bottom}>
+      <View style={styles.row}>
+        <Button text="clear" />
+        <Button text="pow" />
+        <Button text="swap" />
+        <Button text="/" />
+      </View>
+      <View style={styles.row}>
+        <Button text="7" />
+        <Button text="8" />
+        <Button text="9" />
+        <Button text="X" />
+      </View>
+      <View style={styles.row}>
+        <Button text="4" />
+        <Button text="5" />
+        <Button text="6" />
+        <Button text="-" />
+      </View>
+      <View style={styles.row}>
+        <Button text="1" />
+        <Button text="2" />
+        <Button text="3" />
+        <Button text="+" />
+      </View>
+      <View style={styles.row}>
+        <Button text="0" />
+        <Button text="." />
+        <Button text="enter" special />
+      </View>
+    </View>
   </View>
 );
 
---
+++ Button.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const baseContainer = {
+  backgroundColor: '#333A42',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderRightWidth: 1,
+  borderColor: '#70A649',
+};
+
+const baseText = {
+  fontSize: 26,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    ...baseContainer,
+  },
+  specialContainer: {
+    flex: 2,
+    ...baseContainer,
+    backgroundColor: '#70A649',
+  },
+  text: {
+    ...baseText,
+    color: '#70A649',
+  },
+  specialText: {
+    ...baseText,
+    color: '#fff',
+  },
+});
+
+const Button = ({ text, special }) => (
+  <View style={special ? styles.specialContainer : styles.container}>
+    <Text style={special ? styles.specialText : styles.text}>{text}</Text>
+  </View>
+);
+
+export default Button;
